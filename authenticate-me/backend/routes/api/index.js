49046e202cc75b27cb471903c3c09474931069dd
Michael DiGiovanni <midigi@comcast.net>
2021-01-23 15:41:26 -0500

    phase 3 complete

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,9 +1,39 @@
 const router = require('express').Router();
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth, restoreUser  } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+// Test routes for auth
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
 
 
 module.exports = router;
