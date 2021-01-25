093ea93d2d74a22c653e0befff1b13223b5796a9
Michael DiGiovanni <midigi@comcast.net>
2021-01-24 20:10:38 -0500

    phase 2 complete

--- authenticate-me/frontend/src/store/session.js
+++ authenticate-me/frontend/src/store/session.js
@@ -37,6 +37,20 @@ export const restoreUser = () => async dispatch => {
     return res;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    dispatch(setUser(response.data.user));
+    return response;
+  };
+
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
