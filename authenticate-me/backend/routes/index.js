7c3d7c70ec3cb3598b1006092be8dcec26cf35bd
Michael DiGiovanni <midigi@comcast.net>
2021-01-25 00:39:30 -0500

    script cahnges

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -5,12 +5,6 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
-  });
-}
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
@@ -34,9 +28,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 module.exports = router;
