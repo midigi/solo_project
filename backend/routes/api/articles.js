0dee55fcc6064f17ae14c62e35641c88f2debd4f
Michael DiGiovanni <midigi@comcast.net>
2021-01-27 13:49:40 -0500

    backend routes updated

--- backend/routes/api/articles.js
+++ backend/routes/api/articles.js
@@ -8,4 +8,16 @@ router.get('/', asyncHandler(async (req, res)=>{
     return res.json({articles});
 }))
 
+router.post('/createArticle', asyncHandler(async (req, res) => {
+    const user= await db.User.findOne({where: {id: req.session.auth.user_id}});
+    const article = await UserArticles.build({
+        user_id: user.id,
+        title: req.body.title,
+        blurb: req.body.blurb,
+        content: req.body.content
+    })
+    await article.save();
+    res.redirect('/');
+}));
+
 module.exports = router;
